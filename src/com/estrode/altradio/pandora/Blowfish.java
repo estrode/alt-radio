e03e940bd35b9a356dbd80df4174e4553fe9034b
Eric Strode <estrode@usc.edu>
2013-12-19 17:11:08 -0800

    Create skeleton files for blowfish encryption

---
+++ src/com/estrode/altradio/pandora/Blowfish.java
@@ -0,0 +1,5 @@
+package com.estrode.altradio;
+
+public class Blowfish {
+
+}
---
+++ src/com/estrode/altradio/pandora/Pandora.java
@@ -0,0 +1,14 @@
+package com.estrode.altradio;
+
+import org.json.JSONObject;
+
+public class Pandora {
+
+	public void Pandora() {
+		
+	}
+	
+	public JSONObject jsonCall(String method, Boolean https, Boolean blowfish) {
+		return null;
+	}
+}
