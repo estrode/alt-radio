3047d6632d88197cc0c03cddc14c591f299a3cb3
Eric Strode <estrode@usc.edu>
2013-12-19 18:08:55 -0800

    Convert request body to hex after encryption

--- src/com/estrode/altradio/pandora/Pandora.java
+++ src/com/estrode/altradio/pandora/Pandora.java
@@ -5,13 +5,12 @@ import org.json.JSONObject;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.Hex;
 
 public class Pandora {
 	
 	private static String encryptKey = "6#26FRL$ZWD";
 	private static String decryptKey = "R=U!LH$O2B#";
-	private static Base64 base64 = new Base64(true);
 
 	public void Pandora() {
 		
@@ -24,18 +23,20 @@ public class Pandora {
 	//encrypt data using blowfish algorithm
 	public static String encrypt(String data)throws Exception {
 		SecretKeySpec key = new SecretKeySpec(encryptKey.getBytes("UTF8"), "Blowfish");
-		Cipher cipher = Cipher.getInstance("Blowfish");
+		Cipher cipher = Cipher.getInstance("Blowfish/ECB/PKCS5Padding");
 		cipher.init(Cipher.ENCRYPT_MODE, key);
-		return base64.encodeToString(cipher.doFinal(data.getBytes("UTF8")));
+		return Hex.encodeHexString(cipher.doFinal(data.getBytes("UTF8")));
 	}
 
 	//decrypt JSON returned from pandora api using blowfish algorithm
 	public static String decrypt(String data)throws Exception {
-		byte[] encryptedData = base64.decodeBase64(data);
+		byte[] encryptedData = Hex.decodeHex(data.toCharArray());
 		SecretKeySpec key = new SecretKeySpec(decryptKey.getBytes("UTF8"), "Blowfish");
-		Cipher cipher = Cipher.getInstance("Blowfish");
+		Cipher cipher = Cipher.getInstance("Blowfish/ECB/PKCS5Padding");
 		cipher.init(Cipher.DECRYPT_MODE, key);
 		byte[] decrypted = cipher.doFinal(encryptedData);
 		return new String(decrypted); 
 	}
+	
+	
 }
