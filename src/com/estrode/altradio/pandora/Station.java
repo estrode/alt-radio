4535075908a7c73c1f462dd4117f0bf0a5da1f0f
Eric Strode <estrode@usc.edu>
2013-12-21 13:28:58 -0800

    Add station class

---
+++ src/com/estrode/altradio/pandora/Station.java
@@ -0,0 +1,29 @@
+package com.estrode.altradio.pandora;
+
+import org.json.JSONObject;
+
+public class Station {
+
+	private Pandora pandora;
+	private String id;
+	private String idToken;
+	private String name;
+		
+	public Station(Pandora pandora, JSONObject station) {
+		this.pandora = pandora;
+		this.id = station.optString("stationId");
+		this.idToken = station.optString("stationToken");
+		this.name = station.optString("stationName");
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public String getIdToken() {
+		return idToken;
+	}
+	public String getName() {
+		return name;
+	}
+
+}
